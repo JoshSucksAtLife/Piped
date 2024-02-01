b6895c731ee039662a32e28b43cbe20371f056e1
JoshSucksAtLife <132964718+JoshSucksAtLife@users.noreply.github.com>
2024-01-31 21:11:58 -0600

    Rainbow

--- src/components/WatchVideo.vue
+++ src/components/WatchVideo.vue
@@ -422,7 +422,7 @@ export default {
         this.showRecs = !this.getPreferenceBoolean("minimizeRecommendations", false);
         this.showChapters = !this.getPreferenceBoolean("minimizeChapters", false);
         if (this.video?.duration) {
-            document.title = this.video.title + " - Piped";
+            document.title = this.video.title + " - Rainbow";
             this.$refs.videoPlayer.loadVideo();
         }
         window.addEventListener("scroll", this.handleScroll);
@@ -490,7 +490,7 @@ export default {
                 })
                 .then(() => {
                     if (!this.video.error) {
-                        document.title = this.video.title + " - Piped";
+                        document.title = this.video.title + " - Rainbow";
                         this.channelId = this.video.uploaderUrl.split("/")[2];
                         if (!this.isEmbed) this.fetchSubscribedStatus();
 
